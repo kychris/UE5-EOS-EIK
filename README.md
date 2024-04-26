a1ad807e2cf7255c0970c6d0f057e0b044ff6b51
kychris <christiany@meta.com>
2024-04-26 16:07:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Epic_EOS_Test
+
+Developed with Unreal Engine 5
---
+++ Source/Epic_EOS_Test.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Epic_EOS_TestTarget : TargetRules
+{
+	public Epic_EOS_TestTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+
+		ExtraModuleNames.AddRange( new string[] { "Epic_EOS_Test" } );
+	}
+}
